a65969d5f5d499fe5b1bfe18441358cfbfc78708
JesusRuiver <jesusruiver@jesusruiver.com>
2018-04-25 11:33:09 +0200

    Cominicacion entre actividades

---
+++ app/src/main/java/jesus/example/org/caminosdesantiagojesus/GestionMunicipios.java
@@ -0,0 +1,17 @@
+package jesus.example.org.caminosdesantiagojesus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by JesúsRuiver on 25/04/2018.
+ */
+
+public class GestionMunicipios extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gestion_municipios);
+    }
+}
--- app/src/main/java/jesus/example/org/caminosdesantiagojesus/MainActivity.java
+++ app/src/main/java/jesus/example/org/caminosdesantiagojesus/MainActivity.java
@@ -1,13 +1,25 @@
 package jesus.example.org.caminosdesantiagojesus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnGestionMunicipios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void lanzarGestionMunicipios(View view){
+
+        Intent i = new Intent(this, GestionMunicipios.class);
+        startActivity(i);
     }
 }
